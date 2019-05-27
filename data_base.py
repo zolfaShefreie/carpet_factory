d4b6df6d8aa26f2b608d3a11f64a4b6d62964516
zolfaShefreie <z.shefreie@gmail.com>
2019-05-27 05:12:57 -0700

    :)

--- data_base.py
+++ data_base.py
@@ -8,14 +8,27 @@ class dataBase:
     factory_func=factory_info_action.info_func()
     sale_func=sale_service.sale_service()
     def __init__(self):
-        path="./img"
-        for each in os.listdir(path):
-            new_path=path+"/"+str(each)
-            self.img_path.append(new_path)
-        path="./address"
-        for each in os.listdir(path):
-            new_path=path+"/"+str(each)
-            self.address_path.append(new_path)
+        path = os.getcwd()
+        img_dir=os.path.join(path,"img")
+        if img_dir in os.listdir(path):
+            for each in os.listdir(img_dir):
+                new_path=path+"/"+str(each)
+                self.img_path.append(new_path)
+        else:
+            try:
+                os.mkdir(img_dir)
+            except OSError:
+                pass
+        add_dir=os.path.join(path,"address")
+        if add_dir in os.listdir(path):
+            for each in os.listdir(add_dir):
+                new_path=path+"/"+str(each)
+                self.address_path.append(new_path)
+        else:
+            try:
+                os.mkdir(add_dir)
+            except OSError:
+                pass
             
     def save_new_file(self,file_name,imgOradd):
         pass
