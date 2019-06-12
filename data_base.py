31da8e4e5150edc2d42991d157c7ae5afa53d9a7
zolfaShefreie <z.shefreie@gmail.com>
2019-06-12 08:20:25 -0700

    img_to_matrix and matrix to img and 300*400

--- data_base.py
+++ data_base.py
@@ -2,6 +2,8 @@ import factory_info_action
 import sale_service
 import os
 import ast
+from PIL import Image
+import numpy as np
 
 class dataBase:
     # paini ham zakhire  kon img_path
@@ -12,6 +14,78 @@ class dataBase:
     sale_func=sale_service.sale_service()
     def __init__(self):
         pass
+    
+    def image_to_matrix(self,file):
+        try:
+            img=Image.open(file)
+        except Exception:
+            pass
+        matrix=np.asanyarray(img, dtype=int)
+        matrix=matrix.tolist()
+        if len(matrix[0][0])==3:
+            for i in range(0,len(matrix)):
+                for j in range(0,len(matrix[0])):
+                    hex_color='{:02x}{:02x}{:02x}'.format( matrix[i][j][0], matrix[i][j][1] , matrix[i][j][2])
+                    matrix[i][j]=hex_color
+        elif  len(matrix[0][0])==4:
+            for i in range(0,len(matrix)):
+                for j in range(0,len(matrix[0])):
+                    hex_color='{:02x}{:02x}{:02x}{:02x}'.format( matrix[i][j][0], matrix[i][j][1] , matrix[i][j][2], matrix[i][j][3] )
+                    matrix[i][j]=hex_color
+        
+        return matrix
+    
+    def fill_with_black(self,matrix=[[0]]):
+        #300 * 400
+        if len(matrix)==0:
+            raise Exception("the input isn't matrix")
+        elif len(matrix[0])==0:
+            raise Exception("it isn't 2d matrix")
+        black_list=[]
+        for i in range (0,len(matrix)):
+            for j in range(len(matrix[0]),401):
+                matrix[i].append("000000")
+        
+        for  i in range(0,401):
+            black_list.append("000000")
+        
+        for i in range(len(matrix),301):
+            matrix.append(black_list)
+        
+        return matrix
+            
+    def matrix_to_image(self,matrix):
+        #deraieha bayad be sorate hex o string bashan
+        if len(matrix)==0:
+            raise Exception("the input isn't matrix")
+        elif len(matrix[0])==0:
+            raise Exception("it isn't 2d matrix")
+        
+        if len(matrix[0][0])==6:
+        
+            for each in range(0,len(matrix)):
+                for each1 in range(0,len(matrix[0])):
+                    hex_color=matrix[each][each1]
+                    rgb_color=list(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+                    matrix[each][each1]=rgb_color
+
+#             matrix=np.array(matrix,dtype=np.uint8)
+#             img = Image.fromarray(matrix, 'RGB')
+#             img.save('my.jpg') 
+            
+        elif len(matrix[0][0])==8:
+            for each in range(0,len(matrix)):
+                for each1 in range(0,len(matrix[0])):
+                    hex_color=matrix[each][each1]
+                    rgb_color=list(int(hex_color[i:i+2], 16) for i in (0, 2, 4, 6))
+                    matrix[each][each1]=rgb_color
+                    
+        matrix=np.array(matrix,dtype=np.uint8)
+        img = Image.fromarray(matrix, 'RGB')
+        img.save('my.jpg')
+        
+        
+        
     
     def load(self):
 #         path = os.getcwd()
